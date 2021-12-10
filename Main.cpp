575a8f578b2481030a43f07ec4e7165915336be5
Andrey Loskutov <joofer.sp@gmail.com>
2021-12-10 19:46:40 +0300

    Added abstract Connection class

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,7 @@
 // 1. Add machine picking menu                                // Done
 // 2. Add an ability to create a new machine in-runtime       // Done
 // 3. Add purchases logging                                   // Done
-// 4. Restructure ConnectionHandler class                     //
+// 4. Restructure ConnectionHandler class                     // Done
 // 5. Wallets via hash files and database                     //
 // 6. Add error logging switcher using define                 //
 
